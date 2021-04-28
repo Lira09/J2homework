4ce83d012bb0cfe3851e8fbdfc6bc30371676e43
Lira09 <nazirbekova.lira@gmail.com>
2021-04-28 18:54:44 +0600

    Initial commit

---
+++ app/src/main/java/com/geek/j2homework/MainActivity.java
@@ -0,0 +1,28 @@
+package com.geek.j2homework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button Button;
+        Button button = (Button) findViewById(R.id.sing_in);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
